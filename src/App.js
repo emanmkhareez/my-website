b9409ebec8d825a97f173c22dca340f572bead6d
emanmkhareez <emkhareez19@gmail.com>
2021-06-25 02:29:15 -0700

    cm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
+import React from 'react'
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { FaGithub } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import {FaHome } from "react-icons/fa";
+import {FaUserAlt } from "react-icons/fa";
+import {FaPhone } from "react-icons/fa";
+import {FaRProject } from "react-icons/fa";
+import img from './imgs.jpg'
+import Project from './component/Project';
+import Data from './component/Data.json'
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Caveat:wght@400;700&family=Pacifico&display=swap');
+</style>
+
+class  App extends React.Component {
+constructor(props){
+  super(props)
+  this.state={
+    show:false,
+    
+  }
+}
+ 
+  render(){
+    const projectFun= () => {
+    this.setState({
+      show:true,
+      
+    })
+  
+    
+
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+   
+    <div className="container ">
+     
+      <ul>
+
+      <img  className="img"src={img}></img>
+
+      <h5 style={{textAlign:"center",marginTop:2,marginLeft:18}}>Eman Mkhareez</h5>
+
+      <a href="https://github.com/emanmkhareez" ><FaGithub className="icon" /></a>
+      <a><FaInstagramSquare className="icon"/></a>
+      <a href="#"><FaFacebookF className="icon"/></a>
+      <a><FaLinkedinIn className="icon"/></a>
+
+      <li><a href="#"><FaHome/> Home</a></li>  
+      <li><a href="#"  onClick={projectFun}><FaRProject  /> Project</a></li> 
+        <li><a href="#"><FaUserAlt/>  About Us</a></li>
+      
+        <li><a href="#"><FaPhone/> Contact Us</a></li>
+        
+
+      </ul>
+     
+
+      {/* if the show ==false render the project component else render the div that have class ="body"  */}
+
+    <div className="body">
+      <div><img src="https://images.unsplash.com/photo-1616004655123-818cbd4b3143?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" className="imgBody"></img></div>
+      <h2 className="myname">Eman Almkareez</h2>
+      <h4 className="programmerfresh">programmer fresh</h4>
+      </div>
+       <h4 className="footer">designed by:<span style={{color:' rgb(75, 70, 70)'}}>eman mkhareez</span></h4>
+
+</div>
+{this.state.show?
+Data.map((item)=>{
+  return(
+    <Project
+    projectName
+projectName={item.projectName}
+Describe={item.Describe}
+img1={item.img1}
+img2={item.img2}
+
+
+
+    />
+  )
+}):null}
+       </>
+   
+  )
+  }
 }
 
 export default App;
---
+++ src/component/Project.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Data from './Data.json'
+
+
+
+class Project extends React.Component {
+    constructor(props){
+        super(props);
+        
+    }
+    render() {
+        return (
+            <>
+            
+            <div className="project"><br></br>
+                
+                   
+                    <h2 className="projectName"> {this.props.projectName}</h2>
+                    <div className="Describe">
+                        {this.props.Describe}
+                    </div>
+                    <div>
+                        <img src={this.props.img1}className="imgProject"></img>
+                        <img src={this.props.img2}className="imgProject"></img>
+                        <img src={this.props.img3} className="imgProject"></img>
+                    </div>
+
+       
+        
+             
+
+            </div>
+            </>
+        )
+    }
+}
+export default Project
